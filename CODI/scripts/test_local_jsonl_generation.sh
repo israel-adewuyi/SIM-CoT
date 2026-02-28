3fd3dedb8a2203ba24aa2bff8c7ba469a18b8294
Israel Adewuyi <isistickz@gmail.com>
2026-02-28 10:02:28 +0300

    upd

--- CODI/scripts/test_local_jsonl_generation.sh
+++ CODI/scripts/test_local_jsonl_generation.sh
@@ -9,19 +9,19 @@ python test.py \
     --data_name "local-jsonl" \
     --data_path "$DATA_PATH" \
     --output_dir "$OUTPUT_DIR" \
-    --model_name_or_path meta-llama/Llama-3.2-1B-Instruct \
+    --model_name_or_path Qwen/Qwen3-4B \
     --seed 11 \
-    --model_max_length 512 \
+    --model_max_length 16384 \
     --bf16 \
-    --lora_r 128 --lora_alpha 32 --lora_init \
-    --batch_size 64 \
+    --lora_r 16 --lora_alpha 32 --lora_init \
+    --batch_size 1 \
     --greedy True \
-    --num_latent 6 \
+    --num_latent 3 \
     --use_prj True \
     --prj_dim 2048 \
     --prj_no_ln False \
     --prj_dropout 0.0 \
-    --inf_latent_iterations 6 \
+    --inf_latent_iterations 3 \
     --inf_num_iterations 1 \
     --remove_eos True \
     --use_lora True \
